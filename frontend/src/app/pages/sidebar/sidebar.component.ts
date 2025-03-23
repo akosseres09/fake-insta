482710780ccfd7ed3307720d1ec733a2264c01ac
akosseres09 <akosseres75@gmail.com>
2025-03-23 16:03:53 +0100

    more sidebar actions

--- frontend/src/app/pages/sidebar/sidebar.component.ts
+++ frontend/src/app/pages/sidebar/sidebar.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaterialModule } from '../../material.module';
+import { RouterModule } from '@angular/router';
+import { MatMenu } from '@angular/material/menu';
 
 @Component({
     selector: 'app-sidebar',
     standalone: true,
-    imports: [CommonModule, MaterialModule],
+    imports: [CommonModule, MaterialModule, RouterModule, MatMenu],
     templateUrl: './sidebar.component.html',
     styleUrl: './sidebar.component.scss',
 })
