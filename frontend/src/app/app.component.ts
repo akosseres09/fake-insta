8032e909044eddc23e0bc0b89267f06a12c04842
akosseres09 <akosseres75@gmail.com>
2025-03-23 14:23:54 +0100

    configure application and routes in app component

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MaterialModule } from './material.module';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
     selector: 'app-root',
+    standalone: true,
+    imports: [CommonModule, MaterialModule, RouterOutlet],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
 })
-export class AppComponent {
-    title = 'frontend';
-}
+export class AppComponent {}
--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,62 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'app/feed',
+    },
+    {
+        path: 'app',
+        loadComponent: () =>
+            import('./layouts/main-layout/main-layout.component').then(
+                (c) => c.MainLayoutComponent
+            ),
+        children: [
+            {
+                path: 'feed',
+                loadComponent: () =>
+                    import('./pages/feed/feed.component').then(
+                        (c) => c.FeedComponent
+                    ),
+            },
+            {
+                path: 'profile',
+                loadComponent: () =>
+                    import('./pages/profile/profile.component').then(
+                        (c) => c.ProfileComponent
+                    ),
+            },
+        ],
+    },
+    {
+        path: 'auth',
+        loadComponent: () =>
+            import('./layouts/auth-layout/auth-layout.component').then(
+                (c) => c.AuthLayoutComponent
+            ),
+        children: [
+            {
+                path: 'login',
+                loadComponent: () =>
+                    import('./pages/login/login.component').then(
+                        (c) => c.LoginComponent
+                    ),
+            },
+            {
+                path: 'register',
+                loadChildren: () =>
+                    import('./pages/signup/signup.component').then(
+                        (c) => c.SignupComponent
+                    ),
+            },
+        ],
+    },
+    {
+        path: '**',
+        loadComponent: () =>
+            import('./pages/not-found/not-found.component').then(
+                (c) => c.NotFoundComponent
+            ),
+    },
+];
