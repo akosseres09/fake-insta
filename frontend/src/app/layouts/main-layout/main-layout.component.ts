e0e5ef374cc5624cc7b20a6e0fcd8d00ebf412b7
akosseres09 <akosseres75@gmail.com>
2025-03-25 18:37:14 +0100

    layouts

--- frontend/src/app/layouts/main-layout/main-layout.component.ts
+++ frontend/src/app/layouts/main-layout/main-layout.component.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from '../../pages/header/header.component';
 import { FooterNavComponent } from '../../pages/footer-nav/footer-nav.component';
 import { SidebarComponent } from '../../pages/sidebar/sidebar.component';
+import { AuthService } from '../../shared/auth/auth.service';
+import { User } from '../../shared/model/User';
+import { Subscription } from 'rxjs';
 
 @Component({
     selector: 'app-main-layout',
@@ -15,6 +18,23 @@ import { SidebarComponent } from '../../pages/sidebar/sidebar.component';
     templateUrl: './main-layout.component.html',
     styleUrl: './main-layout.component.scss',
 })
-export class MainLayoutComponent {
+export class MainLayoutComponent implements OnInit, OnDestroy {
     currentView = 'feed';
+
+    user: User | null = null;
+    userSubscription: Subscription | null = null;
+
+    constructor(private authService: AuthService) {}
+
+    ngOnInit(): void {
+        this.userSubscription = this.authService
+            .getUser('67e1726d3964575fd8451940')
+            .subscribe((user) => {
+                this.user = user as User;
+            });
+    }
+
+    ngOnDestroy(): void {
+        this.userSubscription?.unsubscribe();
+    }
 }
