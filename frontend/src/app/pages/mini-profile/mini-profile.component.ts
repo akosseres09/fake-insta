7de6696a58612b861e759806f71fa3fb928842cb
akosseres09 <akosseres75@gmail.com>
2025-04-04 21:42:51 +0200

    mini profile fix

--- frontend/src/app/pages/mini-profile/mini-profile.component.ts
+++ frontend/src/app/pages/mini-profile/mini-profile.component.ts
@@ -1,15 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AuthService } from '../../shared/services/auth/auth.service';
+import { User } from '../../shared/model/User';
+import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'app-mini-profile',
-    imports: [],
+    imports: [CommonModule],
     templateUrl: './mini-profile.component.html',
     styleUrl: './mini-profile.component.scss',
 })
-export class MiniProfileComponent {
-    user = {
-        username: 'John Doe',
-        name: 'your_username',
-        avatar: 'https://i.pravatar.cc/150?img=1',
-    };
+export class MiniProfileComponent implements OnInit, OnDestroy {
+    userSubscription: Subscription | null = null;
+    user: User | null = null;
+    avatar: string | null = 'https://i.pravatar.cc/150?img=1';
+    constructor(private authService: AuthService) {}
+
+    ngOnInit(): void {
+        this.userSubscription = this.authService.user$.subscribe({
+            next: (user) => {
+                this.user = user;
+            },
+            error: (err) => {
+                console.error(err);
+            },
+        });
+    }
+
+    ngOnDestroy(): void {
+        this.userSubscription?.unsubscribe();
+    }
 }
