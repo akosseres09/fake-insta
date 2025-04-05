f1bca35533bc58a57cb04f274b9f1323861a2e47
akosseres09 <akosseres75@gmail.com>
2025-04-05 14:05:38 +0200

    authservice update

--- frontend/src/app/shared/services/auth/auth.service.ts
+++ frontend/src/app/shared/services/auth/auth.service.ts
@@ -1,7 +1,8 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { User } from '../../model/User';
-import { BehaviorSubject, Observable, tap } from 'rxjs';
+import { BehaviorSubject, tap } from 'rxjs';
+import { IResponse } from '../../model/Response';
 
 @Injectable({
     providedIn: 'root',
@@ -13,10 +14,38 @@ export class AuthService {
 
     constructor(private http: HttpClient) {}
 
+    setUser(user: User) {
+        this.userSubject.next(user);
+    }
+
     getUser(id: string) {
         return this.http
             .get(`http://localhost:3000/user/${id}`)
             .pipe(tap((user) => this.userSubject.next(user as User)));
+    }
+
+    updateUser(user: User) {
+        const body = new URLSearchParams();
+        body.set('_id', user._id);
+        body.set('email', user.email);
+        body.set('first', user.name.first);
+        body.set('last', user.name.last);
+        body.set('username', user.username);
+        body.set('bio', user.bio ?? '');
+        body.set('profilePictureUrl', user.profilePictureUrl ?? '');
+
+        const headers = new HttpHeaders({
+            'Content-Type': 'application/x-www-form-urlencoded',
+        });
+
+        return this.http.post<IResponse<User | string>>(
+            `http://localhost:3000/update/${user._id}`,
+            body,
+            {
+                headers: headers,
+                withCredentials: true,
+            }
+        );
     }
 
     checkId() {
