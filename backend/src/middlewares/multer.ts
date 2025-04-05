e80089e3ccad4d5fa56aa9ea658f9c63c113557c
akosseres09 <akosseres75@gmail.com>
2025-04-05 21:11:00 +0200

    adding multer and cloudinary config

---
+++ backend/src/middlewares/multer.ts
@@ -0,0 +1,17 @@
+import { Request } from 'express';
+import multer from 'multer';
+import { CloudinaryStorage } from 'multer-storage-cloudinary';
+import cloudinary from '../utils/cloudinary';
+
+const storage = new CloudinaryStorage({
+    cloudinary: cloudinary,
+    params: async (req: Request, file: Express.Multer.File) => ({
+        folder: 'fake-instagram/posts',
+        resourcre_type: file.mimetype.startsWith('video') ? 'video' : 'image',
+        public_id: `${Date.now()}-${file.originalname}`,
+    }),
+});
+
+const upload = multer({ storage });
+
+export default upload;
---
+++ backend/src/utils/cloudinary.ts
@@ -0,0 +1,9 @@
+import { v2 as cloudinary } from 'cloudinary';
+
+cloudinary.config({
+    cloud_name: 'derjx1sje',
+    api_key: '253622751276513',
+    api_secret: 'EFT191NhJKOBIaGg27yXHquYu-c',
+});
+
+export default cloudinary;
