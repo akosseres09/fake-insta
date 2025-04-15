91f6944d3ac003e88d3998d81402bacaebc1e1e0
akosseres09 <akosseres75@gmail.com>
2025-04-15 16:02:15 +0200

    added thumbnail generation for videos

--- frontend/src/app/pages/profile/profile-tabs/profile-tabs.component.ts
+++ frontend/src/app/pages/profile/profile-tabs/profile-tabs.component.ts
@@ -13,4 +13,25 @@ import { Post } from '../../../shared/model/Post';
 })
 export class ProfileTabsComponent {
     @Input() userPosts: Array<Post> | null = null;
+
+    getThumbnail(videoUrl: string): string {
+        // Check if it's a Cloudinary video URL
+        if (!videoUrl.includes('/video/upload/')) {
+            return videoUrl;
+        }
+
+        // Inject the Cloudinary transformation options
+        const transformedUrl = videoUrl.replace(
+            '/video/upload/',
+            '/video/upload/so_1,c_fill,w_300,h_300/' //so=1  start offset at 1 seconds
+        );
+
+        // Replace file extension with .jpg
+        const lastDotIndex = transformedUrl.lastIndexOf('.');
+        if (lastDotIndex !== -1) {
+            return transformedUrl.substring(0, lastDotIndex) + '.jpg';
+        }
+
+        return transformedUrl + '.jpg';
+    }
 }
