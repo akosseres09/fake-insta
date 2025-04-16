190b519906ffd29a289f07e69b0fd57ebe54df5a
akosseres09 <akosseres75@gmail.com>
2025-04-16 12:25:15 +0200

    fixing error when deleting localstorage item

--- frontend/src/app/layouts/main-layout/main-layout.component.ts
+++ frontend/src/app/layouts/main-layout/main-layout.component.ts
@@ -5,7 +5,7 @@ import { FooterNavComponent } from '../../pages/footer-nav/footer-nav.component'
 import { SidebarComponent } from '../sidebar/sidebar.component';
 import { AuthService } from '../../shared/services/auth/auth.service';
 import { User } from '../../shared/model/User';
-import { Subscription } from 'rxjs';
+import { Observable, of, Subscription, switchMap, tap } from 'rxjs';
 import { CommonModule } from '@angular/common';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
@@ -30,33 +30,42 @@ export class MainLayoutComponent implements OnInit, OnDestroy {
     idSubscription?: Subscription;
     logoutSubscription?: Subscription;
     id: string | null = null;
+    idObservable: Observable<string> = new Observable<string>();
 
     constructor(private authService: AuthService, private router: Router) {}
 
     ngOnInit(): void {
         this.id = localStorage.getItem('user');
 
-        if (!this.id) {
-            this.idSubscription = this.authService.checkId().subscribe({
-                next: (id) => {
+        if (this.id) {
+            this.idObservable = of(this.id);
+        } else {
+            this.idObservable = this.authService.checkId().pipe(
+                tap((id) => {
                     this.id = id as string;
                     localStorage.setItem('user', this.id);
-                },
-                error: (error) => {
-                    console.log(error);
-                },
-            });
+                })
+            );
         }
 
-        this.userSubscription = this.authService
-            .getUser(this.id as string)
+        this.idSubscription = this.idObservable
+            .pipe(
+                switchMap((id) => {
+                    const currentUser = this.authService.getCurrentUser();
+                    if (currentUser) {
+                        return of(currentUser);
+                    } else {
+                        return this.authService.getUser(id as string);
+                    }
+                })
+            )
             .subscribe({
                 next: (user) => {
                     this.user = user as User;
+                    this.authService.setUser(this.user);
                 },
                 error: (error) => {
                     console.log(error);
-                    this.router.navigateByUrl('/not-found');
                 },
             });
     }
