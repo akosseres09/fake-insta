48643b8f1197bcb92a63eda68ad4325ed72f59d1
akosseres09 <akosseres75@gmail.com>
2025-04-18 17:12:56 +0200

    fix

--- frontend/src/app/pages/post/post.component.ts
+++ frontend/src/app/pages/post/post.component.ts
@@ -1,6 +1,8 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaterialModule } from '../../material.module';
+import { Post } from '../../shared/model/Post';
+import { User } from '../../shared/model/User';
 
 @Component({
     selector: 'app-post',
@@ -9,8 +11,13 @@ import { MaterialModule } from '../../material.module';
     templateUrl: `./post.component.html`,
     styleUrl: './post.component.scss',
 })
-export class PostComponent {
-    @Input() post: any;
+export class PostComponent implements OnInit {
+    @Input() post?: Post;
+    user?: User;
+
+    ngOnInit(): void {
+        this.user = this.post?.userId as User;
+    }
 
     likePost() {}
 }
